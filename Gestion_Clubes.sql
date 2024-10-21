b0b651ab8ae8e49d44e69b4784f67ee8d812601f
juan pablo di mauro <juanpablodimauro@gmail.com>
2024-10-21 01:09:20 -0300

    agregue la query con la creacion de la base de datos y sus tablas como primer folleto

---
+++ Gestion_Clubes.sql
@@ -0,0 +1,117 @@
+create database Gestion_Clubes
+
+create table Categoria(
+	IdCategoria tinyint primary key identity(1,1),
+	nombre varchar (30) not null,
+)
+
+create table paises(
+	IdPais tinyint primary key identity (1,1),
+	Nombre Varchar (30) not null,
+)
+
+create table persona(
+	IdPersona bigint primary key identity(1,1),
+	Nombre varchar(30) not null,
+	Apellido varchar(30) not null,
+	FechaNacimiento date not null,
+	LugarNacimiento varchar(50) not null,
+	Email varchar (30) not null,
+)
+
+create table EstadoJugador(
+	IdEstadoJugador tinyint primary key identity (1,1),
+	nombre varchar(30) not null,
+)
+
+create table jugador(
+	IdJugador int primary key identity(1,1),
+	IdPersona bigint foreign key references persona(idpersona),
+	Altura tinyint not null check(altura > 150),
+	peso decimal not null check (peso > 60),
+	posicion varchar (15) not null,
+	Idcategoria tinyint foreign key references Categoria (IdCategoria),
+	IdEstadoJugador tinyint foreign key references EstadoJugador (IdEstadojugador),
+)
+
+create table nacionalidad(
+	IdPais tinyint foreign key references paises(IdPais),
+	IdPersona bigint foreign key references persona (IdPersona),
+	primary key (Idpais, Idpersona),
+)
+
+create table entrenador(
+	IdEntrandor int primary key identity (1,1),
+	IdPersona bigint foreign key references persona (IdPersona),
+	Rol varchar (30) not null,
+)
+
+create table socio (
+	IdSocio bigint primary key identity(1,1),
+	IdPersona bigint foreign key references persona (IdPersona),
+)
+
+create table EstadoEntrenamiento(
+	IdEstadoEntrenamiento tinyint primary key identity(1,1),
+	nombre varchar(30) not null,
+)
+
+create table entrenamiento(
+	IdEntrenamiento bigint primary key identity (1,1),
+	FechaHora datetime not null,
+	Duracion time not null,
+	IdCategoria tinyint foreign key references Categoria(IdCategoria),
+	Descripcion varchar(100) not null,
+	Observaciones varchar(100),
+	IdEstadoEntrenamiento tinyint foreign key references EstadoEntrenamiento(IdEstadoEntrenamiento),
+)
+
+create table Asistencia (
+	IdAsistenia bigint primary key identity (1,1),
+	IdEntrenamiento bigint foreign key references entrenamiento (IdEntrenamiento),
+	IdJugador int foreign key references Jugador (idjugador),
+	Asistio bit not null,
+	Observaciones varchar (30),
+)
+
+create table Reporte(
+	IdReporte bigint primary key identity (1,1),
+	IdEntrenamiento bigint foreign key references entrenamiento (IdEntrenamiento),
+	Descrpcion varchar (100) not null,
+	Observaciones varchar (100),
+)
+
+create table TipoIncidencia(
+	IdTipoIncidencia tinyint primary key identity (1,1),
+	nombre varchar(30) not null,
+)
+
+create table incidencia (
+	IdIncidencia int primary key identity (1,1),
+	IdJugador int foreign key references jugador (idjugador),
+	Descripcion varchar(100) not null,
+	FechaRegistro date not null, 
+	FechaResolucion date,
+	IdTipoIncidencia tinyint foreign key references  tipoIncidencia(IdTipoIncidencia),
+	Estado bit not null,
+)
+
+create table TipoUsuario(
+	IdTipoUsuario tinyint primary key identity(1,1),
+	nombre varchar (30) not null,
+)
+
+create table Usuario (
+	IdUsuario bigint primary key identity(1,1),
+	Nombre varchar(50) not null,
+	Contraseña varchar(50) not null,
+	Email varchar (50) not null,
+	IdTipoUsuario tinyint foreign key references TipoUsuario(IdTipoUsuario),
+)
+
+create table Notificacion(
+	IdNotificacion int primary key identity (1,1),
+	IdUsuario bigint foreign key references Usuario (IdUsuario),
+	Mensaje varchar (300) not null,
+	FechaEnvio date not null,
+)
--- Negocio/AccesoDatos.cs
+++ Negocio/AccesoDatos.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Negocio
 {
-    internal class AccesoDatos
+    public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private  SqlCommand comando;
+        private SqlDataReader Lector;
+
+        public SqlDataReader lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS02; database=Gestion_Clubes; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                Lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if(lector != null) 
+                lector.Close();
+            conexion.Close();
+        }
     }
 }
