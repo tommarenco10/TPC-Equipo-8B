ef02795e499418f817e33127233ed43e0f1888cb
SantiGiussani <santi.giussani@gmail.com>
2024-11-04 00:14:12 -0300

    Aclaración de los SP que no se pueden crear con la BDD actual

--- SQLQuery - Procedimientos Almacenados.sql
+++ SQLQuery - Procedimientos Almacenados.sql
@@ -23,16 +23,26 @@ set @IdPersona = SCOPE_IDENTITY()
 insert into jugador values (@IdPersona, @Altura, @Peso, @Posicion, @IdCategoria, @IdEstadoJugador)
 end
 
-create procedure Listar_Jugador
+GO
+
+create or alter procedure Eliminar_Jugador 
+@IdJugador bigint 
 as
 begin
-select j.IdJugador ,p.Nombre, p.Apellido, p.FechaNacimiento, p.pais, p.provincia, p.ciudad, p.Email,
-		j.Altura, j.peso, j.posicion, c.IdCategoria, c.nombre as NombreCategoria, ej.IdEstadoJugador ,ej.nombre as EstadoJugador 
-from persona p
-inner join jugador j on j.IdPersona = p.IdPersona
-inner join Categoria c on c.IdCategoria = j.Idcategoria
-inner join EstadoJugador ej on ej.IdEstadoJugador = j.IdEstadoJugador
+	delete from jugador where IdJugador = @IdJugador
 end
+
+GO
+
+CREATE PROCEDURE Eliminar_Socio
+    @IdSocio INT
+AS
+BEGIN
+    DELETE FROM Socio
+    WHERE IdSocio = @IdSocio;
+END;
+
+GO
 
 create or alter procedure FiltroAvanzado 
 @NombreCategoria varchar(30),
@@ -47,6 +57,21 @@ inner join Categoria c on c.IdCategoria = j.Idcategoria
 inner join EstadoJugador ej on ej.IdEstadoJugador = j.IdEstadoJugador
 where c.nombre like @NombreCategoria and ej.nombre like @NombreEstado
 end
+
+GO
+
+create procedure Listar_Jugador
+as
+begin
+select j.IdJugador ,p.Nombre, p.Apellido, p.FechaNacimiento, p.pais, p.provincia, p.ciudad, p.Email,
+		j.Altura, j.peso, j.posicion, c.IdCategoria, c.nombre as NombreCategoria, ej.IdEstadoJugador ,ej.nombre as EstadoJugador 
+from persona p
+inner join jugador j on j.IdPersona = p.IdPersona
+inner join Categoria c on c.IdCategoria = j.Idcategoria
+inner join EstadoJugador ej on ej.IdEstadoJugador = j.IdEstadoJugador
+end
+
+GO
 
 create or alter procedure Modificar_Jugador 
 @IdJugador bigint, 
@@ -75,53 +100,7 @@ end
 
 GO
 
-create or alter procedure Eliminar_Jugador 
-@IdJugador bigint 
-as
-begin
-	delete from jugador where IdJugador = @IdJugador
-end
-
-GO
-
-CREATE PROCEDURE Agregar_Entrenador
-    @Nombre NVARCHAR(50),
-    @Apellido NVARCHAR(50),
-    @FechaNacimiento DATE,
-    @Ciudad NVARCHAR(50),
-    @Provincia NVARCHAR(50),
-    @Pais NVARCHAR(50),
-    @Rol NVARCHAR(50)
-AS
-BEGIN
-    DECLARE @IdLugarNacimiento INT, @IdPersona INT;
-
-    
-    INSERT INTO LugarNacimiento (Ciudad, Provincia, Pais)
-    VALUES (@Ciudad, @Provincia, @Pais);
-    SET @IdLugarNacimiento = SCOPE_IDENTITY();
-
-    
-    INSERT INTO Persona (Nombre, Apellido, FechaNacimiento, IdLugarNacimiento)
-    VALUES (@Nombre, @Apellido, @FechaNacimiento, @IdLugarNacimiento);
-    SET @IdPersona = SCOPE_IDENTITY();
-
-    
-    INSERT INTO Entrenador (IdPersona, Rol)
-    VALUES (@IdPersona, @Rol);
-END;
-
-GO
-
-CREATE PROCEDURE Eliminar_Entrenador
-    @IdEntrenador INT
-AS
-BEGIN
-    DELETE FROM Entrenador
-    WHERE IdEntrenador = @IdEntrenador;
-END;
-
-GO
+--PROCEDIMIENTOS QUE NO SE PUEDEN CREAR CON LA BDD COMO ESTÁ
 
 CREATE PROCEDURE Actualizar_Entrenador
     @IdEntrenador INT,
@@ -135,10 +114,8 @@ CREATE PROCEDURE Actualizar_Entrenador
 AS
 BEGIN
     DECLARE @IdPersona INT, @IdLugarNacimiento INT;
-
     
     SELECT @IdPersona = IdPersona FROM Entrenador WHERE IdEntrenador = @IdEntrenador;
-
     
     SELECT @IdLugarNacimiento = IdLugarNacimiento FROM Persona WHERE IdPersona = @IdPersona;
     UPDATE LugarNacimiento
@@ -146,46 +123,16 @@ BEGIN
         Provincia = @Provincia,
         Pais = @Pais
     WHERE IdLugarNacimiento = @IdLugarNacimiento;
-
     
     UPDATE Persona
     SET Nombre = @Nombre,
         Apellido = @Apellido,
         FechaNacimiento = @FechaNacimiento
     WHERE IdPersona = @IdPersona;
-
     
     UPDATE Entrenador
     SET Rol = @Rol
     WHERE IdEntrenador = @IdEntrenador;
-END;
-
-GO
-
-CREATE PROCEDURE Agregar_Socio
-    @Nombre NVARCHAR(50),
-    @Apellido NVARCHAR(50),
-    @FechaNacimiento DATE,
-    @Ciudad NVARCHAR(50),
-    @Provincia NVARCHAR(50),
-    @Pais NVARCHAR(50)
-AS
-BEGIN
-    DECLARE @IdLugarNacimiento INT, @IdPersona INT;
-
-    -
-    INSERT INTO LugarNacimiento (Ciudad, Provincia, Pais)
-    VALUES (@Ciudad, @Provincia, @Pais);
-    SET @IdLugarNacimiento = SCOPE_IDENTITY();
-
-    
-    INSERT INTO Persona (Nombre, Apellido, FechaNacimiento, IdLugarNacimiento)
-    VALUES (@Nombre, @Apellido, @FechaNacimiento, @IdLugarNacimiento);
-    SET @IdPersona = SCOPE_IDENTITY();
-
-    
-    INSERT INTO Socio (IdPersona)
-    VALUES (@IdPersona);
 END;
 
 GO
@@ -201,10 +148,8 @@ CREATE PROCEDURE Actualizar_Socio
 AS
 BEGIN
     DECLARE @IdPersona INT, @IdLugarNacimiento INT;
-
     
     SELECT @IdPersona = IdPersona FROM Socio WHERE IdSocio = @IdSocio;
-
    
     SELECT @IdLugarNacimiento = IdLugarNacimiento FROM Persona WHERE IdPersona = @IdPersona;
     UPDATE LugarNacimiento
@@ -212,7 +157,6 @@ BEGIN
         Provincia = @Provincia,
         Pais = @Pais
     WHERE IdLugarNacimiento = @IdLugarNacimiento;
-
     
     UPDATE Persona
     SET Nombre = @Nombre,
@@ -223,12 +167,61 @@ END;
 
 GO
 
-CREATE PROCEDURE Eliminar_Socio
-    @IdSocio INT
+CREATE PROCEDURE Agregar_Entrenador
+    @Nombre NVARCHAR(50),
+    @Apellido NVARCHAR(50),
+    @FechaNacimiento DATE,
+    @Ciudad NVARCHAR(50),
+    @Provincia NVARCHAR(50),
+    @Pais NVARCHAR(50),
+    @Rol NVARCHAR(50)
 AS
 BEGIN
-    DELETE FROM Socio
-    WHERE IdSocio = @IdSocio;
+    DECLARE @IdLugarNacimiento INT, @IdPersona INT;
+    
+    INSERT INTO LugarNacimiento (Ciudad, Provincia, Pais)
+    VALUES (@Ciudad, @Provincia, @Pais);
+    SET @IdLugarNacimiento = SCOPE_IDENTITY();
+    
+    INSERT INTO Persona (Nombre, Apellido, FechaNacimiento, IdLugarNacimiento)
+    VALUES (@Nombre, @Apellido, @FechaNacimiento, @IdLugarNacimiento);
+    SET @IdPersona = SCOPE_IDENTITY();
+    
+    INSERT INTO Entrenador (IdPersona, Rol)
+    VALUES (@IdPersona, @Rol);
 END;
 
 GO
+
+CREATE PROCEDURE Agregar_Socio
+    @Nombre NVARCHAR(50),
+    @Apellido NVARCHAR(50),
+    @FechaNacimiento DATE,
+    @Ciudad NVARCHAR(50),
+    @Provincia NVARCHAR(50),
+    @Pais NVARCHAR(50)
+AS
+BEGIN
+    DECLARE @IdLugarNacimiento INT, @IdPersona INT;
+
+    INSERT INTO LugarNacimiento (Ciudad, Provincia, Pais)
+    VALUES (@Ciudad, @Provincia, @Pais);
+    SET @IdLugarNacimiento = SCOPE_IDENTITY();
+
+    INSERT INTO Persona (Nombre, Apellido, FechaNacimiento, IdLugarNacimiento)
+    VALUES (@Nombre, @Apellido, @FechaNacimiento, @IdLugarNacimiento);
+    SET @IdPersona = SCOPE_IDENTITY();
+    
+    INSERT INTO Socio (IdPersona)
+    VALUES (@IdPersona);
+END;
+
+GO
+
+CREATE PROCEDURE Eliminar_Entrenador
+    @IdEntrenador INT
+AS
+BEGIN
+    DELETE FROM Entrenador
+    WHERE IdEntrenador = @IdEntrenador;
+END;
