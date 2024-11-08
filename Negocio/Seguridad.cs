f3c9af39d2f55377ed085c9ef48e2827cc7cfb44
unknown <tommarenco10@gmail.com>
2024-11-08 18:28:06 -0300

    Creacion de la clase Seguridad para verificar si hay sesiones activas y de que tipo de usuario se trata.

---
+++ Negocio/Seguridad.cs
@@ -0,0 +1,94 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class Seguridad
+    {
+
+
+
+        public bool sesionActiva(Object user)
+        {
+
+            Usuario usuario = user != null ? (Usuario)user : null;
+            
+            if (usuario != null)
+            {
+                return true;
+
+            }
+            else { return false; }
+        }
+
+
+
+        public bool esAdmin(Object user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if (usuario!=null&&usuario.Tipo == TipoUsuario.Administrador)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+        public bool esEntrenador(Object user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if (usuario != null && usuario.Tipo== TipoUsuario.CuerpoTecnico)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+
+
+        public bool esMedico(Usuario user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if (usuario != null && usuario.Tipo == TipoUsuario.CuerpoMedico)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+
+        public bool esSocio(Usuario user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if (usuario != null && usuario.Tipo == TipoUsuario.Socio)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+        public bool esHincha(Usuario user)
+        {
+            Usuario usuario = user != null ? (Usuario)user : null;
+            if (usuario != null && usuario.Tipo == TipoUsuario.Hincha)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+
+
+
+    }
+}
+
+
